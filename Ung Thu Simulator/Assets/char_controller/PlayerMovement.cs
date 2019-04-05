d00a8368eae29a0aa928973b66210891c682fdf5
Kha Tran <1653032@hcmus.edu.vn>
2019-04-05 07:16:55 +0700

    First commit

---
+++ Ung Thu Simulator/Assets/char_controller/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+
+	float horizontalMove = 0f;
+	bool verticalMove = false;
+	public float runspeed = 40f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runspeed;
+		
+		if(Input.GetButtonDown("Jump")){
+			verticalMove = true;
+		}
+	}
+
+	private void FixedUpdate() {
+		controller.Move(horizontalMove * Time.fixedDeltaTime,false,verticalMove);
+		verticalMove = false;
+	}
+}
